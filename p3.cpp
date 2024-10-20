f18eeb2f76220abfed84379b5934051d6eaa96e6
prasagit99 <saumya20011124@gmail.com>
2024-10-20 20:57:41 +0530

    Add files via upload

---
+++ p3.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+using namespace std;
+int main ()
+{
+    cout << " Saumya " << endl;
+    cout << " Student " << endl;
+    cout << " University Of Vavuniya " << endl;
+
+    return 0;
+}
